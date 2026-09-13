83518ffe988135ea715d16ffcb4e788a6185e0d5
Marcblack08 <137033747+Marcblack08@users.noreply.github.com>
2026-09-13 00:46:04 -0500

    Invalidate M.A.R.C. PWA asset cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='marc-pwa-v21-live-assets';
+const CACHE_NAME='marc-pwa-v23-live-assets';
 const APP_SHELL=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icons/marc.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
